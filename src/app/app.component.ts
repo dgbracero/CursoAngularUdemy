a2171ae3631556205e6833dd64d800c228fde1de
dgbracero <diegobracero@hotmail.com>
2020-11-15 14:05:26 -0500

    170 Crear Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'aprendiendo-angular';
+  public title = 'Master de Javascript y Angular';
 }
