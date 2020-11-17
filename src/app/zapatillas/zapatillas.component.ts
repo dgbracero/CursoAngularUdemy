210f5c686193e193e6ed86251e5e156b4b52f034
dgbracero <diegobracero@hotmail.com>
2020-11-17 11:54:20 -0500

    ngfor antes de borrar duplicados

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -9,17 +9,31 @@ import { Zapatilla } from '../models/zapatilla';
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de zapatillas";
     public zapatillas: Array<Zapatilla>;
+    public marcas: String[];
 
     constructor(){
+        this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Nike Airmax', 'Nike', 'Rojas', 40, true),
             new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true),
             new Zapatilla('Nike Runner MD', 'Nike', 'Negras', 60, true),
-            new Zapatilla('Adidas Yezzy', 'adidas', 'Blanco', 180, false)
+            new Zapatilla('Adidas Yezzy', 'Adidas', 'Blanco', 180, false)
         ];
     }
     ngOnInit(){
         console.log(this.zapatillas);
+        this.getMarcas();
     }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla,index) => {
+            this.marcas.push(zapatilla.marca);
+            console.log(index);
+        });
+
+        console.log(this.marcas);
+    }
+
+
 
 }
