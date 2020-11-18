49cead1f92ffe3317335983383552442bc0e8b92
dgbracero <diegobracero@hotmail.com>
2020-11-18 15:01:12 -0500

    191 Redirecciones router navigate

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -22,7 +22,15 @@ export class CursosComponent implements OnInit {
       this.nombre = params.nombre;
       this.followers = params.followers;    
       //this.nombre = params['nombre'];
+
+      if(this.nombre == 'ninguno'){
+        this._router.navigate(['/home']);
+      }
     })
+  }
+
+  redirigir(){
+    this._router.navigate(['/zapatillas']);
   }
 
 }
