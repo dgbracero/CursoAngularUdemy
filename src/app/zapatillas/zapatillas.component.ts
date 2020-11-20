fa8f244a2f88511b6035c24ea880ccd5c354106f
dgbracero <diegobracero@hotmail.com>
2020-11-20 15:13:24 -0500

    194 Crear servicios

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Zapatilla } from '../models/zapatilla';
+import { ZapatillaService } from '../services/zapatilla.service';
 
 @Component({
     selector: 'zapatillas',
-    templateUrl: './zapatillas.component.html'
+    templateUrl: './zapatillas.component.html',
+    providers: [ZapatillaService]
 })
 
 export class ZapatillasComponent implements OnInit{
@@ -14,18 +16,16 @@ export class ZapatillasComponent implements OnInit{
     public mi_marca: string;
 
 
-    constructor(){
+    constructor(
+        private _zapatillaService:ZapatillaService   
+    ){
         this.color = 'blue';
         this.marcas = new Array();
-        this.zapatillas = [
-            new Zapatilla('Nike Airmax', 'Nike', 'Rojas', 40, true),
-            new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true),
-            new Zapatilla('Nike Runner MD', 'Nike', 'Negras', 60, true),
-            new Zapatilla('Adidas Yezzy', 'Adidas', 'Blanco', 180, false)
-        ];
+        
     }
     ngOnInit(){
-        //console.log(this.zapatillas);
+        this.zapatillas = this._zapatillaService.getZapatillas();
+
         this.getMarcas();
     }
 
